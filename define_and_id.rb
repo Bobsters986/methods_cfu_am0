5bc2a79fee6bb8d1895a57c0c8453693d17f702c
Bobby Luly <bobbyluly@gmail.com>
2022-11-03 12:41:41 -0600

    Add answers

--- define_and_id.rb
+++ define_and_id.rb
@@ -5,23 +5,33 @@
 # The downcase method is called on the string object "Hello World"
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
-"Hello World".downcase
+p "Hello World".downcase
 
 # YOU DO the rest:
 
 
-"Hello World".include?("Hello")
+p "Hello World".include?("Hello")
+# The include method is called on the string object "Hello World"
+# The argument "Hello" is passed; include asks if the string includes the word "Hello"
+# The return value is true
 
+p "Hello World".end_with?("Hello")
+# The end_with? method is called on the string object "Hello World"
+# The argument "Hello" is passed; end_with? asks if the string ends with the word "Hello"
+# The return value is false
 
-"Hello World".end_with?("Hello")
+p "Hello World".end_with?("rld")
+# The end_with?  method is called on the string object "Hello World"
+# The argument "rld" is passed; end_with? asks if the string ends with the letters "rld"
+# The return value is true
 
+p 12.even?
+# The even? method is called on the integer 12
+# No arguments are passed; even? asks if the integer 12 is even
+# The return value is true
 
-"Hello World".end_with?("rld")
-
-
-12.even?
-
-
-18.next
-
+p 18.next
+# The next method is called on the integer 18
+# No arguments are passed; next tells it to print the next integer after 18
+# The return value is 19
 
